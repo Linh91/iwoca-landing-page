0606b9ab17b75dcfad71d89d7471d19a349bc681
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-07-17 19:01:58 +0100

    Added basic layout to page

---
+++ script.js
@@ -0,0 +1,15 @@
+// jQuery(function($){
+// 							$(document).ready(function(){
+// 								$("#main-title-list").find("li").css("opacity","1");
+// 								$("#main-title-list")
+// 									.vTicker(
+// 									{
+// 										speed: 400,
+// 										showItems: 1,
+// 										pause: 1500,
+// 										mousePause : false,
+// 										direction: "up",
+// 									}
+// 								);
+// 							});
+// 						});
