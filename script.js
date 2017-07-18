0c87d2694788bb51dcbac37d2f5d1cd1c73f5da7
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-07-18 22:15:49 +0100

    Test form is complete

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Validating Empty Field
 function check_empty() {
-if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
-alert("Fill All Fields !");
+if (document.getElementById('name').value == "" || document.getElementById('telephone').value == "") {
+alert("Please do not leave Name and Telephone field empty");
 } else {
 document.getElementById('form').submit();
-alert("Form Submitted Successfully...");
+alert("Thank you. An expert will contact you soon.");
 }
 }
 //Function To Display Popup
 function div_show() {
-document.getElementById('abc').style.display = "block";
+document.getElementById('requestForm').style.display = "block";
 }
 //Function to Hide Popup
 function div_hide(){
-document.getElementById('abc').style.display = "none";
+document.getElementById('requestForm').style.display = "none";
 }
